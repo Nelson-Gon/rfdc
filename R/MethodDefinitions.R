ce669af826f347752b7207607fa037cc74e9e758
Nelson-Gon <gonzabato@hotmail.com>
2020-03-02 16:27:22 +0800

    Methods

---
+++ R/MethodDefinitions.R
@@ -0,0 +1,33 @@
+# Method Definitions for Class FoodSearch
+setGeneric("get_food_info", function(object,...) standardGeneric("get_food_info"))
+setMethod("get_food_info", signature("FoodSearch"),
+          function(object,target=NULL,
+                   page_number=1,
+                   require_all=TRUE,
+                   sort_field="publishedDate",
+                   sort_direction="asc"){
+            
+          
+            
+        search_query_list =   list(generalSearchInput = object@search_phrase,
+                 ingredients = object@ingredients,
+                 requireAllWords=require_all,
+                 pageNumber=page_number,
+                 sortField=sort_field,
+                 sortDirection = sort_direction,
+                 brandOwner = object@brand_owner)
+    
+ 
+        #return(httr::add_headers(`api_key` = object@api_key))
+        
+        res_response<-httr::POST(paste0("https://api.nal.usda.gov/fdc/v1/search?api_key=",
+                          object@api_key),
+                   body = search_query_list, encode = "json",
+                   httr::add_headers("Content-Type"="application/json"))
+        
+        # Make res more manipulatable
+      res<-jsonlite::fromJSON(httr::content(res_response,"text"))
+      
+      res$foods[[target]]
+            
+            })
